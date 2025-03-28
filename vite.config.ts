db77dfed38aacfc535cefa1667d6240264a4c291
AK <37541753+avnkrlo@users.noreply.github.com>
2025-03-28 15:18:19 +0400

    404 pages flowbite

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react_portfolio",
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
