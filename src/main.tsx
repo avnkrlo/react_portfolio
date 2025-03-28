b6fcce16cab4be50e94d9b3889ae051e64887122
AK <37541753+avnkrlo@users.noreply.github.com>
2025-03-28 21:16:13 +0400

    install react-router and added pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,45 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home.tsx';
+import Work from './pages/Work.tsx';
+import Project from './pages/Project.tsx';
+import About from './pages/About.tsx';
+import Booking from './pages/Booking.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/react_portfolio/",
+    element: <App/>,
+    children: [
+      {
+        path: "/react_portfolio/",
+        element: <Home/>
+      },
+      {
+        path: "/react_portfolio/work",
+        element: <Work/>
+      },
+      {
+        path: "/react_portfolio/project",
+        element: <Project/>
+      },
+      {
+        path: "/react_portfolio/about",
+        element: <About/>
+      },
+      {
+        path: "/react_portfolio/booking",
+        element: <Booking/>
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
