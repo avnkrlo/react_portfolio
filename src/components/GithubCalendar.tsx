0d5a90e866802c2a3ff8274f1d153206ce059e2b
Aivan Karlo <aivang1029@gmail.com>
2025-06-03 23:08:28 +0400

    embed GitCalendar needs fix

---
+++ src/components/GithubCalendar.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { GitHubCalendar } from "github-contribution-calendar";
+
+// const App = () => (
+//   <div>
+//     <h1>My GitHub Contributions</h1>
+//     <GitHubCalendar
+//       username="github-username"
+//       token="YOUR_GITHUB_ACCESS_TOKEN"
+//       year={2024} // Optional: defaults to current year
+//       showLabels={true} // Optional
+//       fontSize={14} // Optional
+//       theme="forest" // Optional : defaults to "forest"
+//     />
+//   </div>
+// );
+
+// export default App;
