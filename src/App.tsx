f5df5cb0cc0713a413f017b9243b3a2fc9cf1f38
Aivan Karlo <aivang1029@gmail.com>
2025-06-03 18:43:22 +0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import NavbarComponent from "./components/NavbarComponent";
 import { Outlet } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="bg-black">
       <NavbarComponent/>
+        <Home/>
         <Outlet/>
       <Footer/>
     </div>
