2526808b843ce9bac8936df3c5899a4fdf996d28
Aivan Karlo <37541753+avnkrlo@users.noreply.github.com>
2025-06-04 05:43:51 +0000

    add github calendar

--- src/components/GithubCalendar.tsx
+++ src/components/GithubCalendar.tsx
@@ -1,18 +1,7 @@
-// import React from "react";
-// import { GitHubCalendar } from "github-contribution-calendar";
+import GithubCalendar from "react-github-calendar";
 
-// const App = () => (
-//   <div>
-//     <h1>My GitHub Contributions</h1>
-//     <GitHubCalendar
-//       username="github-username"
-//       token="YOUR_GITHUB_ACCESS_TOKEN"
-//       year={2024} // Optional: defaults to current year
-//       showLabels={true} // Optional
-//       fontSize={14} // Optional
-//       theme="forest" // Optional : defaults to "forest"
-//     />
-//   </div>
-// );
-
-// export default App;
+export default function GitHubCalendar() {
+    return(
+        <GithubCalendar username="avnkrlo"/>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import GitHubCalendar from "../components/GithubCalendar";
+
 export default function Home() {
     return (
         <>
             <div className="mt-15">
-                <div className="pt-5 text-white">
+                <div className="place-items-center max-w-screen-lg pt-5 text-white">
                     Home
+
+                    <GitHubCalendar/>
                 </div>
             </div>
         </>
