950a3da03b4d42ceeb367bdf06dc90277636935e
Aivan Karlo <37541753+avnkrlo@users.noreply.github.com>
2025-12-03 15:05:25 +0000

    center navbar buttons

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -16,8 +16,8 @@ export default function Jumbotron() {
                 <div className="flex items-center p-3">
                     <div className="w-full">
                         <Link to="mailto:aivan.karlo28@gmail.com?subject=Let%27s catch up and talk about opportunity!" type="button" 
-                            className="inline-flex items-center px-5 py-5 text-md text-left text-white bg-blue-700 border border-blue-500 rounded-lg center-center hover:text-white sm:ms-4 hover:bg-transparent focus:ring-4 focus:ring-blue-700">
-                                Get in touch
+                            className="inline-flex items-center px-5 py-5 text-md text-left text-white bg-blue-700 border border-blue-500 rounded-lg hover:bg-white/10 hover:backdrop-blur-md hover:border-white/30 transition-all duration-300 ease-out
+                                focus:ring-4 focus:ring-blue-700">Get in touch
 
                                 <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4"/>
