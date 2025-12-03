a4d4a06df8c6e83c6316c3a1740936e21d476915
Aivan Karlo <37541753+avnkrlo@users.noreply.github.com>
2025-12-03 09:03:42 +0000

    navbar touchup

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Clipboard from "./Clipboard";
 import TypingTextComponent from "./TypingTextComponent";
-import BtnDownloadCV from "./BtnDownloadCV";
 
 export default function Jumbotron() {
     return (
--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -11,7 +11,7 @@ export default function NavbarComponent() {
       </NavbarBrand>
 
       <div className="flex md:order-2">
-          <Link to="/react_portfolio/booking" className="py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+          <Link to="/react_portfolio/booking" className="items-center px-3 py-2 text-md text-center text-gray-900 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:text-white sm:ms-4"
               >Book a Call
           </Link>
           
