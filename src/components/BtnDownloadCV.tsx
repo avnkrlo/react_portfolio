79c67329d1daf23c802e2cc6b82ef1359c3af86e
Aivan Karlo <37541753+avnkrlo@users.noreply.github.com>
2025-12-03 11:46:49 +0000

    fix cv download btn - no file

--- src/components/BtnDownloadCV.tsx
+++ src/components/BtnDownloadCV.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import Cv from '../assets/cv/Resume - Aivan Karlo Geronimo.pdf';
 import { HiOutlineDocumentDownload } from "react-icons/hi";
 
 export default function btnDownloadCV() {
     return (
-        <Link to={`${import.meta.env.BASE_URL}Aivan Karlo Geronimo.pdf.pdf`} target='_blank' download
-            className="inline-flex items-center px-6 py-2 text-md text-left text-white bg-blue-700 border border-blue-500 rounded-lg space-x-5 hover:text-white sm:ms-4 hover:bg-transparent focus:ring-4 focus:ring-blue-700">
+        <Link to={Cv} target='_blank' download
+            className="inline-flex items-center px-4 py-2 text-md text-left text-white bg-blue-700 border border-blue-500 rounded-lg space-x-5 hover:text-white sm:ms-4 hover:bg-transparent focus:ring-4 focus:ring-blue-700">
                 <HiOutlineDocumentDownload/> Download CV
         </Link>
     );
