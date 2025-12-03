eff0f344f30627e799788a61293f6ab9c8998e9b
Aivan Karlo <37541753+avnkrlo@users.noreply.github.com>
2025-12-03 07:15:25 +0000

    fix download btn

--- src/components/BtnDownloadCV.tsx
+++ src/components/BtnDownloadCV.tsx
@@ -3,7 +3,8 @@ import { HiOutlineDocumentDownload } from "react-icons/hi";
 
 export default function btnDownloadCV() {
     return (
-        <Link to={`${import.meta.env.BASE_URL}RESUME_AIVAN_KARLO_GERONIMO.pdf`} target='_blank' download className="px-6 py-3 backdrop-blur-md bg-white/10 border border-blue-400 text-blue-400 rounded-md hover:bg-blue-500/20 transition">
+        <Link to={`${import.meta.env.BASE_URL}Aivan Karlo Geronimo.pdf.pdf`} target='_blank' download 
+        className="inline-flex items-center px-5 py-5 text-md text-left text-white bg-blue-700 border border-blue-500 rounded-lg center-center hover:text-white sm:ms-4 hover:bg-transparent focus:ring-4 focus:ring-blue-700">
             <HiOutlineDocumentDownload/>Download CV
         </Link>
     );
